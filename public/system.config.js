49e9fbedd2aad530833a2344b52fc5efb8095b28
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2023-04-03 18:56:36 +0300

    fixed build for gh-pages

--- public/system.config.js
+++ public/system.config.js
@@ -1,8 +1,7 @@
 SystemJS.config({
   map: {
-    "vkey": "../node_modules/vkey/index.js",
-    "events": "../node_modules/eventemitter3/index.js",
-    "game-inputs": "../node_modules/game-inputs/inputs.js",
-    "tone": "../node_modules/tone/build/Tone.js",
+    "vkey": "https://unpkg.com/vkey@1.0.0/index.js",
+    "events": "https://unpkg.com/eventemitter3@4.0.0/index.js",
+    "game-inputs": "https://unpkg.com/game-inputs@0.4.0/inputs.js",
   }
 });
